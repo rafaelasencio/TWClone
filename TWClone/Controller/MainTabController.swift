db8483f0d16e95d480e8f789de84bce3e4ed1e58
Rafael AB <rafaelasenciodeveloper@gmail.com>
2020-11-30 12:37:57 +0100

    Set root VC programmatically

---
+++ TWClone/Controller/MainTabController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabController.swift
+//  TWClone
+//
+//  Created by RafaelAsencio on 30/11/2020.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+    
+
+}
