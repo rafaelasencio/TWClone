0166eb45dbf668875ea62465ebc993000fef222d
Rafael AB <rafaelasenciodeveloper@gmail.com>
2020-11-30 14:29:32 +0100

    Add assets. Add images for tabItems and Embed controllers in Navigation

--- TWClone/Controller/MainTabController.swift
+++ TWClone/Controller/MainTabController.swift
@@ -22,12 +22,23 @@ class MainTabController: UITabBarController {
     //MARK: - Helpers
     
     private func configureVCs(){
-        let feed = FeedController()
-        let explore = ExploreController()
+        let feed          = FeedController()
+        let explore       = ExploreController()
         let notifications = NotificationController()
         let conversations = ConversationsController()
+        let nav1 = templateNavigationController(imageName: "home_unselected", rootVC: feed)
+        let nav2 = templateNavigationController(imageName: "search_unselected", rootVC: explore)
+        let nav3 = templateNavigationController(imageName: "search_unselected", rootVC: notifications)
+        let nav4 = templateNavigationController(imageName: "search_unselected", rootVC: conversations)
         
-        viewControllers = [feed, explore, notifications, conversations]
+        viewControllers = [nav1, nav2, nav3, nav4]
+    }
+    
+    private func templateNavigationController(imageName: String, rootVC: UIViewController)->UINavigationController{
+        let nav = UINavigationController(rootViewController: rootVC)
+        nav.tabBarItem.image = UIImage(named: imageName)
+        nav.navigationBar.tintColor = .white
+        return nav
     }
 
 }
