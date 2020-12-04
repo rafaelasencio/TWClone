b9cd4996d046a392d84c2dafa5fb808f8e0d0815
Rafael AB <rafaelasenciodeveloper@gmail.com>
2020-12-04 12:53:36 +0100

    Add UI components to TweetCell

--- TWClone/View/TweetCell.swift
+++ TWClone/View/TweetCell.swift
@@ -9,12 +9,118 @@ import UIKit
 
 class TweetCell: UICollectionViewCell {
     
+    //MARK: - Properties
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let captionLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 16)
+        lbl.numberOfLines = 0
+        lbl.text = "some text"
+        return lbl
+    }()
+    
+    private lazy var commentButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(UIImage(named: "comment"), for: .normal)
+        btn.tintColor = .darkGray
+        btn.setDimensions(width: 20, height: 20)
+        btn.addTarget(self, action: #selector(handleCommentTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var retweetButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(UIImage(named: "retweet"), for: .normal)
+        btn.tintColor = .darkGray
+        btn.setDimensions(width: 20, height: 20)
+        btn.addTarget(self, action: #selector(handleCommentTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var likeButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(UIImage(named: "like"), for: .normal)
+        btn.tintColor = .darkGray
+        btn.setDimensions(width: 20, height: 20)
+        btn.addTarget(self, action: #selector(handleCommentTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private lazy var shareButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(UIImage(named: "share"), for: .normal)
+        btn.tintColor = .darkGray
+        btn.setDimensions(width: 20, height: 20)
+        btn.addTarget(self, action: #selector(handleShareTapped), for: .touchUpInside)
+        return btn
+    }()
+    
+    private let infoLabel = UILabel()
+    
+    //MARK: - Lifecycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .twitterBlue
+        backgroundColor = .white
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 8)
+        
+        let stack = UIStackView(arrangedSubviews: [infoLabel, captionLabel])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 4
+        addSubview(stack)
+        stack.anchor(top: profileImageView.topAnchor, left: profileImageView.rightAnchor, right: rightAnchor,
+                     paddingLeft: 12, paddingRight: 12)
+        infoLabel.font = UIFont.systemFont(ofSize: 12)
+        infoLabel.text = "Elon Musk @elon"
+        
+        let actionStack = UIStackView(arrangedSubviews: [commentButton, retweetButton, likeButton, shareButton])
+        actionStack.axis = .horizontal
+        actionStack.distribution = .fillProportionally
+        actionStack.spacing = 72
+        addSubview(actionStack)
+        actionStack.centerX(inView: self)
+        actionStack.anchor(bottom: bottomAnchor, paddingBottom: 8)
+//        actionStack.anchor(top: captionLabel.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor,
+//                           paddingLeft: 40, paddingBottom: 4, paddingRight: 40)
+        
+        let underlineView = UIView()
+        underlineView.backgroundColor = .systemGroupedBackground
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor,
+                             right: rightAnchor, height: 1)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    //MARK: - Selectors
+    @objc func handleCommentTapped(){
+        print("handleCommentTapped")
+    }
+    
+    @objc func handleRetweetTapped(){
+        print("handleRetweetTapped")
+    }
+    
+    @objc func handleLikeTapped(){
+        print("handleLikeTapped")
+    }
+    
+    @objc func handleShareTapped(){
+        print("handleShareTapped")
+    }
+    
+    //MARK: - Helpers
 }
