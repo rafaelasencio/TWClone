30a0c630e6caa61f0d2498070fe4936c3b447a7a
Rafael AB <rafaelasenciodeveloper@gmail.com>
2020-12-05 20:06:53 +0100

    Add some UI components for ProfileHeader view. Change barStyle of ProfileController

--- TWClone/View/ProfileHeader.swift
+++ TWClone/View/ProfileHeader.swift
@@ -10,17 +10,108 @@ import UIKit
 class ProfileHeader: UICollectionReusableView {
     
     //MARK: - Properties
+    private lazy var backButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setImage(#imageLiteral(resourceName: "baseline_arrow_back_white_24dp").withRenderingMode(.alwaysOriginal), for: .normal)
+        btn.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return btn
+    }()
     
+    private lazy var containerView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .twitterBlue
+        v.addSubview(backButton)
+        backButton.anchor(top: v.topAnchor, left: v.leftAnchor,
+                 paddingTop: 42, paddingLeft: 16)
+        backButton.setDimensions(width: 30, height: 30)
+        return v
+    }()
+    
+    private var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.backgroundColor = .lightGray
+        iv.layer.borderColor = UIColor.white.cgColor
+        iv.layer.borderWidth = 4
+        return iv
+    }()
+
+    private lazy var editProfileFollowButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitle("Loading", for: .normal)
+        btn.layer.borderColor = UIColor.twitterBlue.cgColor
+        btn.layer.borderWidth = 1.25
+        btn.setTitleColor(.twitterBlue, for: .normal)
+        btn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        btn.addTarget(self, action: #selector(handleEditProfileFollow), for: .touchUpInside)
+        return btn
+    }()
+    
+    private let fullnameLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 20)
+        lbl.text = "Elon Musk"
+        return lbl
+    }()
+    
+    private let usernameLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 16)
+        lbl.text = "@elon"
+        lbl.textColor = .lightGray
+        return lbl
+    }()
+    
+    private let bioLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.font = UIFont.systemFont(ofSize: 16)
+        lbl.numberOfLines = 3
+        lbl.text = "This is a user bio that will span more than one line test purposes"
+        return lbl
+    }()
     
     //MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .red
+        
+        addSubview(containerView)
+        containerView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor,
+                             height: 108)
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: containerView.bottomAnchor, left: leftAnchor,
+                                paddingTop: -24, paddingLeft: 8)
+        profileImageView.setDimensions(width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: containerView.bottomAnchor, right: rightAnchor,
+                                       paddingTop: 12, paddingRight: 12)
+        editProfileFollowButton.setDimensions(width: 100, height: 36)
+        editProfileFollowButton.layer.cornerRadius = 36 / 2
+        
+        let userDetailsStack = UIStackView(arrangedSubviews: [fullnameLabel, usernameLabel, bioLabel])
+        userDetailsStack.axis = .vertical
+        userDetailsStack.distribution = .fill
+        userDetailsStack.spacing = 4
+        addSubview(userDetailsStack)
+        userDetailsStack.anchor(top: profileImageView.bottomAnchor, left: leftAnchor,
+                                right: rightAnchor, paddingTop: 8, paddingLeft: 12,
+                                paddingRight: 12)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - Selectors
+    @objc func handleDismissal(){
+        
+    }
+    
+    @objc func handleEditProfileFollow(){
+        
+    }
 }
